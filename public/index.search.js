6192241988509b9b222d09b9bb9454d45ff08bbc
Faith K Pueneh <puenehfaith@gmail.com>
2023-02-16 00:55:02 +0100

    updated

---
+++ public/index.search.js
@@ -0,0 +1,128 @@
+var relearn_search_index = [
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Introduction",
+    "uri": "/css/introduction/index.html"
+  },
+  {
+    "content": "HTML is a shortened form for hypertext markup language. Its purpose is to structure and organize your website. It is the foundation of your website. It is made up of several elements or tags, that define the various parts of the page, such as headings, paragraphs, and images. To create a complete webpage, HTML is combined with other technologies such as Cascading Style Sheets (CSS) and JavaScript. Tim Berners-Lee created HTML in 1991 with the release of version 1.0. To get you started, you will learn\nThe basics of HTML. Define elements, attributes and other key terms. Show how an HTML page is structured Explain other important basic language features. ",
+    "description": "",
+    "tags": null,
+    "title": "Introduction",
+    "uri": "/html/introduction/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Introduction",
+    "uri": "/javascript/introduction/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "CSS Syntax",
+    "uri": "/css/syntax/index.html"
+  },
+  {
+    "content": "HTML is the markup language used in building webpages. You will learn all about HTML in this documentation.\n",
+    "description": "",
+    "tags": null,
+    "title": "Html",
+    "uri": "/html/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Javascript Syntax",
+    "uri": "/javascript/syntax/index.html"
+  },
+  {
+    "content": "This is why you should learn HTML:\nHTML is the building block of the web. It is the most fundamental language for creating websites. It provides the structure and content that makes up a website. Learning HTML is essential for anyone who wants to create and maintain a website, including web developers, marketers, content creators, and website owners. HTML also provides a basis for more advanced web technologies, such as CSS and JavaScript. ",
+    "description": "",
+    "tags": null,
+    "title": "Why learn HTML",
+    "uri": "/html/why/index.html"
+  },
+  {
+    "content": "In this chapter we would talking about CSS.\n",
+    "description": "",
+    "tags": null,
+    "title": "Css",
+    "uri": "/css/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "CSS Inclusion",
+    "uri": "/css/inclusion/index.html"
+  },
+  {
+    "content": "Before you begin writing HTML code, you must first set up your environment. You will use a text editor to achieve this. A text editor is used to create HTML code and, in some cases, more advanced features that allow developers to effectively create web applications. There are several editors available for writing HTML code. Here are a few examples:\nVisual Studio Code Visual Studio Code is a free source code editor developed by Microsoft for Windows, Linux, and macOS. It includes support for debugging, embedded Git control, syntax highlighting, intelligent code completion, snippets, and code refactoring. The editor is highly extensible for customization with a variety of plugins created by the community. It is also customizable, allowing users to change the theme, keyboard shortcuts, and preferences.\nNotepad++ Notepad++\nSublime Text Sublime Text is a cross-platform source code editor with a Python application programming interface (API). It natively supports many programming languages and markup languages, and functions can be added by users with plugins, typically community-built and maintained under free-software licenses. It is a popular text editor among developers, particularly those working with web technologies such as HTML, CSS, JavaScript, and Python.\n",
+    "description": "",
+    "tags": null,
+    "title": "HTML Setup",
+    "uri": "/html/setup/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Where to place Javascript",
+    "uri": "/javascript/placement/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Colors",
+    "uri": "/css/colors/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Helloworld",
+    "uri": "/html/helloworld/index.html"
+  },
+  {
+    "content": "In this chapter we would talking about Javascript.\n",
+    "description": "",
+    "tags": null,
+    "title": "Javascript",
+    "uri": "/javascript/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Javascript Variables",
+    "uri": "/javascript/variables/index.html"
+  },
+  {
+    "content": "Lorem Ipsum gkkhhjkk\n",
+    "description": "",
+    "tags": null,
+    "title": "Operators",
+    "uri": "/javascript/operators/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
